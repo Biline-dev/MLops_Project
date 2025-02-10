53280f31dd7afd6b290e39ea3564ff5046d54763
Boulmaali Linda Imene <61952652+Biline-dev@users.noreply.github.com>
2025-02-10 07:42:27 +0100

    Update finetune_track_hyperparametre.py

--- training/train/finetune_track_hyperparametre.py
+++ training/train/finetune_track_hyperparametre.py
@@ -9,9 +9,9 @@ from tensorflow.keras import layers, models, optimizers
 from tensorflow.keras.callbacks import EarlyStopping
 
 
-from mlops_project.backend.data_preparation.data_loading_locally import load_local_images_and_preprocess
-from mlops_project.backend.data_preparation.data_loading_s3 import load_images_from_source
-from data_preparation.data_processing import preprocess_and_split_data
+from train.data_preparation.data_loading_locally import load_local_images_and_preprocess
+from train.data_preparation.data_loading_s3 import load_images_from_source
+from train.data_preparation.data_processing import preprocess_and_split_data
 
 # Ensure the project root is accessible
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
